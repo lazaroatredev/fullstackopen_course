a2b71596744c0d0ccc06ef4b08ccf9dae61ba71f
lazaroatredev <lazaroatredev@gmail.com>
2025-03-26 06:45:52 -0400

    resolviendo 2.1: Informacion del curso paso 6

---
+++ part2/courseInfo/src/App.jsx
@@ -0,0 +1,78 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = ({ part, exercise }) => {
+  return (
+    <p>
+      {part} {exercise}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  const [part1, part2, part3] = parts;
+
+  return (
+    <>
+      <Part part={part1.name} exercise={part1.exercises} />
+      <Part part={part2.name} exercise={part2.exercises} />
+      <Part part={part3.name} exercise={part3.exercises} />
+    </>
+  );
+};
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      {/* <Total parts={course.parts} /> */}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const [part1, part2, part3] = parts;
+
+  return (
+    <>
+      <p>
+        Number of exercises{" "}
+        {part1.exercises + part2.exercises + part3.exercises}
+      </p>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;
