fd52da880c650c7220aa88a66fbb358ea4014e17
lazaroatredev <lazaroatredev@gmail.com>
2025-03-27 07:24:56 -0400

    resolviendo 2.8: la agenda telefónica paso 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,28 +1,39 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phoneNumber: 1111111111 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const handleChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
   };
 
-  const isNameExist = (name) =>  persons.some((person => person.name.toLowerCase() === name.toLowerCase()))
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const isNameExist = (name) =>
+    persons.some((person) => person.name.toLowerCase() === name.toLowerCase());
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("submiting...", newName);
     const newPerson = {
       name: newName,
+      phoneNumber: newNumber,
     };
-    if(isNameExist(newName)){
-      alert(`${newName} is already added to phonebook`)
-      setNewName("")
-      return
+    if (isNameExist(newName)) {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
+      return;
     }
-    setPersons(persons.concat(newPerson))
-    setNewName("")
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -30,18 +41,30 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name:{" "}
+          <input value={newName} name="newName" onChange={handleNameChange} />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            name="newNumber"
+            onChange={handleNumberChange}
+            type="number"
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>{persons.map((person) =>(
-        <li key={person.name} style={{listStyle: "none"}}>
-          {person.name}
-        </li>
-      ))}</ul>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name} style={{ listStyle: "none" }}>
+            {person.name} - {person.phoneNumber}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
