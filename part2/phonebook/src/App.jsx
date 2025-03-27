47a1b6ec0cba9ec64219034139182a50dce054c5
lazaroatredev <lazaroatredev@gmail.com>
2025-03-27 06:59:53 -0400

    resolviendo 2.6: La Agenda Telefónica Paso 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submiting...", newName);
+    const newPerson = {
+      name: newName,
+    };
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>{persons.map((person) =>(
+        <li key={person.name} style={{listStyle: "none"}}>
+          {person.name}
+        </li>
+      ))}</ul>
+    </div>
+  );
+};
+
+export default App;
