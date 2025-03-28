90a206bd3861c9d42b246735154ad6fe5ea19c0d
lazaroatredev <lazaroatredev@gmail.com>
2025-03-28 05:50:43 -0400

    resolviendo ejercicio 2.11: La Agenda Telefónica paso 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,17 +2,24 @@ import { useState } from "react";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [show, setShow] = useState("");
+
+  const fetchEffect = () => {
+    const fetchPersons = axios.get("http://localhost:3001/persons")
+    fetchPersons.then( (response)=> {
+      const personsArray = response.data
+      setPersons(personsArray)
+    })
+  };
+
+  useEffect(fetchEffect, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -54,7 +61,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter show={show} handleSearch={handleSearch}/>
+      <Filter show={show} handleSearch={handleSearch} />
       <h2>Add a new</h2>
       <PersonForm
         newName={newName}
@@ -64,7 +71,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-     <Persons personsToShow={personsToShow}/>
+      <Persons personsToShow={personsToShow} />
     </div>
   );
 };
